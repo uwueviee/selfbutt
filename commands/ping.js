be10f9b5641c063f9930723bd6a39fafd32a3429
Noculi <merryfurrback@gmail.com>
2017-08-30 00:38:27 -0400

    forgot about the extra webloggers

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,4 @@
 const packageJSON = require('../package.json')
-const fs = require('fs')
-const os = require('os')
-const moment = require('moment')
-
-function webLogger (data) {
-  var time = '[' + moment().format('MMMM Do YYYY, h:mm:ss a')
-  var finalMessage = time + '] ' + data + os.EOL
-  fs.appendFile('./logs.txt', finalMessage, function (err) {
-    if (err) {
-      return webLogger(err)
-    }
-  })
-}
 
 function handler (bot, msg, args) {
   bot.createMessage(msg.channel.id, {
