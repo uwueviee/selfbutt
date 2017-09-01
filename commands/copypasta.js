a12ebc261ea81d361620e048d81b79b49b82673d
Merryfurr <merryfurrback@gmail.com>
2017-08-31 21:25:24 -0400

    Copypasta standard test passing

--- commands/copypasta.js
+++ commands/copypasta.js
@@ -1,18 +1,4 @@
-const packageJSON = require('../package.json')
 const copypastaJSON = require('../commandDeps/copypasta.json')
-const fs = require('fs')
-const os = require('os')
-const moment = require('moment')
-
-function webLogger (data) {
-  var time = '[' + moment().format('MMMM Do YYYY, h:mm:ss a')
-  var finalMessage = time + '] ' + data + os.EOL
-  fs.appendFile('./logs.txt', finalMessage, function (err) {
-    if (err) {
-      return webLogger(err)
-    }
-  })
-}
 
 function handler (bot, msg, args) {
   bot.createMessage(msg.channel.id, copypastaJSON[args])
