85c7d8f1df1b3d4865a64d1e9a036863e907bb8f
Merryfurr <merryfurrback@gmail.com>
2017-09-02 20:21:17 -0400

    Fix issues with it not being standard

--- commands/face.js
+++ commands/face.js
@@ -1,10 +1,10 @@
 const faceJSON = require('../commandDeps/face.json')
 
 function handler (bot, msg, args) {
-  if(faceJSON.hasOwnProperty(args)){
-    bot.createMessage(msg.channel.id, faceJSON[args]) 
+  if (faceJSON.hasOwnProperty(args)) {
+    bot.createMessage(msg.channel.id, faceJSON[args])
   } else {
-  	bot.createMessage(msg.channel.id, "(._.) I don't know what face that is.") 
+    bot.createMessage(msg.channel.id, "(._.) I don't know what face that is.")
   }
 }
 
