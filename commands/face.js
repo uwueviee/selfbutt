ea824ba35b1f3f6a5801ce7dfa2735479839bff5
Noculi <merryfurrback@gmail.com>
2017-09-02 20:16:08 -0400

    Check if face requested is in JSON file and if not, tell user

--- commands/face.js
+++ commands/face.js
@@ -1,7 +1,11 @@
 const faceJSON = require('../commandDeps/face.json')
 
 function handler (bot, msg, args) {
-  bot.createMessage(msg.channel.id, faceJSON[args])
+  if(faceJSON.hasOwnProperty(args)){
+    bot.createMessage(msg.channel.id, faceJSON[args]) 
+  } else {
+  	bot.createMessage(msg.channel.id, "(._.) I don't know what face that is.") 
+  }
 }
 
 module.exports = function (moduleHolder) {
