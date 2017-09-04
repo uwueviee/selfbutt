f678b8b3515b83f265aff7d0ff2e67b636ffca86
Merryfurr <merryfurrback@gmail.com>
2017-09-03 22:07:27 -0400

    songInfo based on Imgur

---
+++ commands/songInfo.js
@@ -0,0 +1,73 @@
+const packageJSON = require('../package.json')
+var config = require('../config.json')
+const songInfoJSON = require('../commandDeps/songInfo.json')
+const fs = require('fs')
+const os = require('os')
+const moment = require('moment')
+const imgur = require('imgur-node-api'),
+  path = require('path')
+
+function webLogger (data) {
+  var time = '[' + moment().format('MMMM Do YYYY, h:mm:ss a')
+  var finalMessage = time + '] ' + data + os.EOL
+  fs.appendFile('./logs.txt', finalMessage, function (err) {
+    if (err) {
+      return webLogger(err)
+    }
+  })
+}
+
+imgur.setClientID(songInfoJSON.clientID)
+
+var location = config.snipLocation
+var image = songInfoJSON.imageLocation
+
+function handler (bot, msg, args) {
+  fs.readFile(location, 'utf8', function (err, data) {
+    if (data === '') {
+      bot.createMessage(msg.channel.id, {
+        embed: {
+          title: 'No song playing',
+          author: {
+            name: msg.author.username,
+            icon_url: msg.author.avatarURL
+          },
+          color: 0x008000,
+          footer: {
+            text: 'SelfButt ' + packageJSON.version + ' by Noculi'
+          }
+        }
+      })
+    } else {
+      imgur.upload(path.join(image), function (err, ree) {
+        webLogger(ree.data.link)
+        fs.readFile(location, 'utf8', function (err, data) {
+          if (err) throw err
+          bot.createMessage(msg.channel.id, {
+            embed: {
+              title: 'Song:',
+              description: data,
+              author: {
+                name: msg.author.username,
+                icon_url: msg.author.avatarURL
+              },
+              color: 0x008000,
+              thumbnail: {
+                url: ree.data.link,
+                height: '700',
+                width: '700'
+              },
+              footer: {
+                text: 'SelfButt ' + packageJSON.version + ' by Noculi'
+              }
+            }
+          })
+        })
+      })
+    }
+  })
+}
+
+module.exports = function (moduleHolder) {
+  moduleHolder['songInfo'] = handler
+}
