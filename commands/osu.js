c87889100f8ce6e1aa245233f41264a9f95640f2
Noculi <merryfurrback@gmail.com>
2017-09-30 11:32:38 -0400

    Better version of osu! command and api addition

--- commands/osu.js
+++ commands/osu.js
@@ -1,5 +1,23 @@
+const packageJSON = require('../package.json')
+
 function handler (bot, msg, args) {
-  bot.createMessage(msg.channel.id, 'http://lemmmy.pw/osusig/sig.php?colour=pink&uname=' + args + '&pp=1&countryrank&removeavmargin&darktriangles&avatarrounding=4&onlineindicator=undefined&xpbar')
+  bot.createMessage(msg.channel.id, {
+    embed: {
+      author: {
+        name: 'osu! Signuature',
+        icon_url: msg.author.avatarURL
+      },
+      image: {
+        url: 'http://lemmmy.pw/osusig/sig.php?colour=pink&uname=' + args + '&pp=1&countryrank&removeavmargin&darktriangles&avatarrounding=4&onlineindicator=undefined&xpbar',
+        height: '94',
+        width: '338'
+      },
+      color: 0x008000,
+      footer: {
+        text: 'SelfButt ' + packageJSON.version + ' by Noculi'
+      }
+    }
+  })
 }
 
 module.exports = function (moduleHolder) {
